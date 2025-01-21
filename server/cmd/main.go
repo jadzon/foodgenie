6fd974d0c4b2017550675dfb970060f2f4970355
jadzon <jacobtoja@icloud.com>
2025-01-21 19:37:16 +0100

    szybki patch

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -21,8 +21,10 @@ func main() {
 	application := app.Init(db, &cfg.App)
 	router := gin.Default()
 	handler := handlers.NewHandler(application)
+	mediaHandler := handlers.NewMediaHandler(application)
 	router.POST("/api/user/register", handler.Register)
 	router.POST("/api/user/login", handler.Login)
+	router.POST("/api/image", mediaHandler.UploadImage)
 	err = router.Run("localhost:8080")
 	if err != nil {
 		log.Fatal("failed to initialize server")
